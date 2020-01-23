f9ea14b4c79e84a74cdae066310c5e2ca4fa69e4
sndnvaps <sndnvaps@gmail.com>
2020-01-23 23:12:33 +0800

    fix gui bug

--- gui/build_x64.cmd
+++ gui/build_x64.cmd
@@ -17,7 +17,8 @@ if "%hour_ten%" == " " (
    set BuildTime=%year%%month%%day%%hour_ten%%hour_one%%minute%%second%
 )
 
-rsrc manifest ebookdownloader_gui.manifest -ico ebookdownloader.ico -arch amd64 -o rsrc.syso
+rsrc -manifest ebookdownloader_gui.manifest -ico ebookdownloader.ico -arch amd64 -o rsrc_x64.syso
 
 go build -ldflags "-H windowsgui -w -s -X main.Commit=%commitid% -X main.BuildTime=%buildtime%" -o ebookdownloader_gui.exe
 copy ebookdownloader_gui.exe ..\
+del  rsrc_x64.syso
