b4cc585d8e12c663293a50ef4bb03c72ab1d1203
sndnvaps <sndnvaps@gmail.com>
2020-02-29 23:16:51 +0800

    更新PowerShell脚本，添加生成编译版本的功能

--- gui/build_x32.ps1
+++ gui/build_x32.ps1
@@ -3,8 +3,14 @@ $origin_date=Get-Date
 $build_time=$origin_date.ToString('yyyy-MM-dd hh:mm:ss')
 $commit_id=git rev-parse --short HEAD
 
+$last_tag_commit_id=git rev-list --tags --max-count=1
+$last_tag=git describe --tags $last_tag_commit_id
+Write-Host "BuildTime = $build_time"
+Write-Host "CommitID = $commit_id"
+Write-Host "last_tag = $last_tag"
+
 rsrc -manifest ebookdownloader_gui.manifest -ico ebookdownloader.ico -arch 386 -o rsrc_x32.syso
 
-go build -ldflags "-H windowsgui -w -s -X main.Commit=$commit_id -X 'main.BuildTime=$build_time' -linkmode external -extldflags '-static'" -o ebookdownloader_gui.exe
+go build -ldflags "-H windowsgui -w -s -X main.Commit=$commit_id -X 'main.BuildTime=$build_time' -X main.Version=$last_tag -linkmode external -extldflags '-static'" -o ebookdownloader_gui.exe
 Copy-Item ebookdownloader_gui.exe ..\
 Remove-Item rsrc_x32.syso
--- http-server/build.ps1
+++ http-server/build.ps1
@@ -3,5 +3,11 @@ $origin_date=Get-Date
 $build_time=$origin_date.ToString('yyyy-MM-dd hh:mm:ss')
 $commit_id=git rev-parse --short HEAD
 
-go build -ldflags "-X main.Commit=$commit_id -X 'main.BuildTime=$build_time'" -o ebookdownloader_http-server.exe
+$last_tag_commit_id=git rev-list --tags --max-count=1
+$last_tag=git describe --tags $last_tag_commit_id
+Write-Host "BuildTime = $build_time"
+Write-Host "CommitID = $commit_id"
+Write-Host "last_tag = $last_tag"
+
+go build -ldflags "-X main.Commit=$commit_id -X 'main.BuildTime=$build_time' -X main.Version=$last_tag" -o ebookdownloader_http-server.exe
 Copy-Item ebookdownloader_http-server.exe ..\
