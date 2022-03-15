ffd3c59a4fa76c049f13da171ad9b14f53ce83fc
sndnvaps <sndnvaps@gmail.com>
2022-03-15 20:45:43 +0800

    update cli version's copyright to 2022

--- cli/ebookdl_cli.go
+++ cli/ebookdl_cli.go
@@ -341,7 +341,7 @@ func main() {
 			Email: "sndnvaps@gmail.com",
 		},
 	}
-	app.Copyright = "© 2019 - 2021 Jimes Yang<sndnvaps@gmail.com>"
+	app.Copyright = "© 2019 - 2022 Jimes Yang<sndnvaps@gmail.com>"
 	app.Usage = "用于下载 笔趣阁(https://www.biqufan.com/ ,http://www.biqugse.com/,https://www.biduoxs.com/, https://www.biquwu.cc/),999小说网(https://www.6zw.net/) ,顶点小说网(https://www.23us.la , https://www.booktxt.net) 上面的电子书，并保存为txt格式或者(mobi格式,awz3格式)的电子书"
 	app.Action = EbookDownloader
 	app.Flags = []cli.Flag{
